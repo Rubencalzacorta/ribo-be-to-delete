63c8fba94810903d75c85599dedec81cbc5aecff
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-04-03 20:42:25 +0200

    FOLDER CHANGE AND INV/BORR REPLACEMENT

---
+++ public/precache-manifest.931d8c698d7601ec91e237176b488898.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "597977a46ee39733e793",
+    "url": "/static/js/main.597977a4.chunk.js"
+  },
+  {
+    "revision": "6fbb2dfe0286dc6afa7f",
+    "url": "/static/js/2.6fbb2dfe.chunk.js"
+  },
+  {
+    "revision": "597977a46ee39733e793",
+    "url": "/static/css/main.f63f016c.chunk.css"
+  },
+  {
+    "revision": "ca342469899e7205251f6ddd727b97e5",
+    "url": "/index.html"
+  }
+];
