bc23322558ae1a624eeaeb467f1091b19d6ce23e
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-05-03 14:42:07 +0200

    ADDED DUE REVIEW CRON

--- bin/dueReview.js
+++ bin/dueReview.js
@@ -1,34 +1,44 @@
 #! /app/bin/node
+
+require('dotenv').config();
+
 const moment = require('moment')
 const LoanSchedule = require('../models/LoanSchedule')
 const Loan = require('../models/Loan')
 const mongoose = require('mongoose')
+const dueReview = async () => {
 
-Loan.find({}).then(console.log)
-const dueReview = () => {
-    
     let begMonth = moment()
     let endMonth = moment().add(30, 'd')
     let queryPending = {date: {$lte: endMonth, $gte: begMonth}, status: 'PENDING'}
     let updateToDue = { $set: {status: 'DUE'} }
     let queryClosedLoans = {status: 'CLOSED'}
     let updateToClosed = { $set: {status: 'CLOSED'} }
-    
-    LoanSchedule.updateMany(queryPending, updateToDue).exec()
-    .then( (e) => {
-        console.log('OVERDUE CRON SCHEDULED SUCCESSFULLY', e)
-        let overdueDate = moment().subtract(7, 'd');
-        let queryDue =  {date: {$lte: overdueDate}, status: 'DUE'}
-        let updateToOverdue = { $set: {status: 'OVERDUE'} }
-        LoanSchedule.updateMany(queryDue, updateToOverdue)}).then( resp => console.log({updated: resp}))    
-    .catch( e => console.log(e))
-    Loan.find(queryClosedLoans).select({ "status": 1, "_id": 1, '_borrower': 0, 'loanSchedule': 0, 'investors': 0})
-        .then( resp => { return resp = resp.map( e => {return e._id})})
-        .then( resp => LoanSchedule.updateMany({_loan: {$in: resp}}, updateToClosed))
-        .then( objList => console.log({Updated: objList}))
-        .catch( e => console.log(e))
+    let overdueDate = moment().subtract(7, 'd');
+    let queryDue =  {date: {$lte: overdueDate}, status: 'DUE'}
+    let updateToOverdue = { $set: {status: 'OVERDUE'} }
+
+    mongoose.connect('mongodb://riboadmin:233586364Mgb.@ds249035.mlab.com:49035/ribo', {
+    auto_reconnect:true,
+    useNewUrlParser: true
+    })
+    .then(() => { console.log(`Connected to Mongo on ${'mongodb://riboadmin:233586364Mgb.@ds249035.mlab.com:49035/ribo'}`)})
+    .then(() => LoanSchedule.updateMany(queryPending, updateToDue))
+    .then( objList => console.log({Updated: objList}))
+    .then( async (e) => {
+        await LoanSchedule.updateMany(queryDue, updateToOverdue).then( resp => console.log({updated: resp}))})   
+    .then( async () => {
+        await Loan.find(queryClosedLoans).select({ "status": 1, "_id": 1, '_borrower': 0, 'loanSchedule': 0, 'investors': 0})
+            .then( resp => { return resp = resp.map( e => {return e._id})})
+            .then( resp => LoanSchedule.updateMany({_loan: {$in: resp}}, updateToClosed))
+            .then( objList => console.log({Updated: objList}))
+    }) 
+    .then( async () => {
+        await mongoose.connection.close()
+    })
+    .catch(err => { console.error('Error connecting to mongo', err)});
 }
 
-// dueReview()
+dueReview()
 
 
