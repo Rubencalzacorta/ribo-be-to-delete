95c1928077660f67719a0078ac6bf5c54af3ae85
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-05-07 19:08:23 +0200

    MOD AND FIXED FEE CONSTANTS

---
+++ public/precache-manifest.aeb64ef778df2d2ce6b2c3596a47cc45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56daa4fab348c0377679",
+    "url": "/static/js/main.025b1336.chunk.js"
+  },
+  {
+    "revision": "0c773da92fdb3154cbb5",
+    "url": "/static/js/2.d8ecb640.chunk.js"
+  },
+  {
+    "revision": "56daa4fab348c0377679",
+    "url": "/static/css/main.0595a0d2.chunk.css"
+  },
+  {
+    "revision": "73d471d8016031d8ac212b158365208b",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ecdd88a99684f1836ee93b0b0c9fe1b5.js"
+  "/precache-manifest.aeb64ef778df2d2ce6b2c3596a47cc45.js"
 );
 
 workbox.clientsClaim();
