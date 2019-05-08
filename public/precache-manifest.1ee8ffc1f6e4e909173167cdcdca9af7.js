6648cc43e31431882da8dc15e670070f31b801ee
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-05-08 19:24:04 +0200

    FIX CAPITAL REMAINING BUG

--- public/precache-manifest.1ee8ffc1f6e4e909173167cdcdca9af7.js
+++ public/precache-manifest.1ee8ffc1f6e4e909173167cdcdca9af7.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "dead9e213d267a6a0d06",
-    "url": "/static/js/main.bfb77b6c.chunk.js"
+    "revision": "ca57982f34c6ff90a394",
+    "url": "/static/js/main.e7555ac5.chunk.js"
   },
   {
     "revision": "0c773da92fdb3154cbb5",
     "url": "/static/js/2.d8ecb640.chunk.js"
   },
   {
-    "revision": "dead9e213d267a6a0d06",
+    "revision": "ca57982f34c6ff90a394",
     "url": "/static/css/main.0595a0d2.chunk.css"
   },
   {
-    "revision": "834beba75d30dd45f8e5ad4cb888c086",
+    "revision": "85ae898827ad90bc7514db4a99f8abad",
     "url": "/index.html"
   }
 ];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e75c41a5327f174681d276930f92dec.js"
+  "/precache-manifest.1ee8ffc1f6e4e909173167cdcdca9af7.js"
 );
 
 workbox.clientsClaim();
