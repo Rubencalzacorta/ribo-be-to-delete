12aa552ccfa8d8a0705e862ed67a21e8bfb88bbd
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-07-15 19:28:36 +0200

    Fix: List search service

--- public/precache-manifest.d2b644db1c71475bff09c2cc3aa924e6.js
+++ public/precache-manifest.d2b644db1c71475bff09c2cc3aa924e6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "a47f6515b4fffd7974bb",
-    "url": "/static/js/main.099a0f3f.chunk.js"
+    "revision": "58dfe1ba7c9ba8edf066",
+    "url": "/static/js/main.fd70a061.chunk.js"
   },
   {
     "revision": "18eb9c2e119caa74e259",
     "url": "/static/js/2.2a45c242.chunk.js"
   },
   {
-    "revision": "a47f6515b4fffd7974bb",
+    "revision": "58dfe1ba7c9ba8edf066",
     "url": "/static/css/main.686dcd07.chunk.css"
   },
   {
-    "revision": "19ccdd1f8c577372478afea1f28ad0a7",
+    "revision": "3efbaf6358ec43419c819cb4261088ed",
     "url": "/index.html"
   }
 ];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dbd4c8bd3e4423e8d89b5baf6a6e97c6.js"
+  "/precache-manifest.d2b644db1c71475bff09c2cc3aa924e6.js"
 );
 
 workbox.clientsClaim();
