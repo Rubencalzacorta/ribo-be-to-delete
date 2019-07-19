b3c3a73a6bbae348eea5953a915abd98d0087419
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-07-19 16:22:19 +0200

    UPDATE: updated libraries

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const favicon = require('serve-favicon');
-const hbs = require('hbs');
+const hbs = require('handlebars');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
@@ -74,7 +74,7 @@ require('./passport')(app);
 
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
