6db29abf6421b80f3d5ac94ec142ceb0f0eebcba
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-08-02 13:29:42 +0200

    Deploy: checkout process improved

--- public/precache-manifest.55b4e3101e7cbfe6d887263481df3b3f.js
+++ public/precache-manifest.55b4e3101e7cbfe6d887263481df3b3f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "ba0f07ab1793108f0e49",
-    "url": "/static/js/main.43303e89.chunk.js"
+    "revision": "cfda8a15de6306421ee9",
+    "url": "/static/js/main.ef66eb7f.chunk.js"
   },
   {
     "revision": "2e96732100e8e91ae417",
     "url": "/static/js/2.5b5fe9aa.chunk.js"
   },
   {
-    "revision": "ba0f07ab1793108f0e49",
-    "url": "/static/css/main.71a4ad46.chunk.css"
+    "revision": "cfda8a15de6306421ee9",
+    "url": "/static/css/main.5f95a580.chunk.css"
   },
   {
-    "revision": "e616d21a04e1cabdb2002d66fc5b6182",
+    "revision": "ccd7e7e35c2793596edf22832e52f660",
     "url": "/index.html"
   }
 ];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a31159bd6936b6bbaf87b251415c8b9.js"
+  "/precache-manifest.55b4e3101e7cbfe6d887263481df3b3f.js"
 );
 
 workbox.clientsClaim();
