feaa225c9c01c764e455844bd31979d0e0f72016
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-09-04 18:27:17 +0200

    Prep deploy with new react scripts

---
+++ public/precache-manifest.4ec4ec2c00811d39badc95aee9fac14f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c438799483649020a47158394bec715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "674b53502758c62ef8db",
+    "url": "/static/css/main.b55edbcd.chunk.css"
+  },
+  {
+    "revision": "ac3918a4d10fc8dcc270",
+    "url": "/static/js/2.97efdb1d.chunk.js"
+  },
+  {
+    "revision": "674b53502758c62ef8db",
+    "url": "/static/js/main.431a6ab1.chunk.js"
+  },
+  {
+    "revision": "83020d3e18a104244f63",
+    "url": "/static/js/runtime~main.8492be3a.js"
+  }
+]);
