2d6a35992ebe12b842414d6aba184bc42fa7baa2
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-10-09 12:58:26 +0200

    schema constants

---
+++ models/constants/index.js
@@ -0,0 +1,31 @@
+module.exports.collateralTypes = [
+    'automobile',
+    'motorcycle',
+    'receivables',
+    'investments',
+    'machinaryAndEquipment',
+    'valuablesAndColletibles',
+    'electronicItems'
+]
+
+module.exports.currentStatus = [
+    'depositedIntoBranch',
+    'collateralWithBorrower',
+    'returnedToBorrower',
+    'reposessionInitiated',
+    'repossed',
+    'underAuction',
+    'sold',
+    'transferedOwnership',
+    'lost',
+    'stolen',
+    'insuranceClaim'
+]
+
+module.exports.condition = [
+    'new',
+    'excellent',
+    'good',
+    'fair',
+    'demaged'
+]
