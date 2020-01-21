1052281dd4e90f0b5eb6069a5eb497f9bd196981
Miguel Gomez Barnola <mgomezb89@gmail.com>
2020-01-21 13:21:19 +0100

    Deploy: tx updated values

---
+++ public/precache-manifest.c8555cb3d3190b4924bf09b93b58b94e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e2018fcd0e831e208420d930c96cac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6b12ecbfd24bc6268cc",
+    "url": "/static/css/main.96eb56ca.chunk.css"
+  },
+  {
+    "revision": "6dcfd34b6530cc015187",
+    "url": "/static/js/2.5f4255b6.chunk.js"
+  },
+  {
+    "revision": "a6b12ecbfd24bc6268cc",
+    "url": "/static/js/main.f93bc7a5.chunk.js"
+  },
+  {
+    "revision": "8b7ccfd7244059ac813b",
+    "url": "/static/js/runtime~main.508fb02e.js"
+  }
+]);
